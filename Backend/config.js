013af5ce345086f58f3515310b800f8a44c90930
mustafa-albash1 <40171130+mustafa-albash1@users.noreply.github.com>
2023-11-19 10:36:54 +0300

    Add files via upload

---
+++ Backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 3000;
+
+export const mongoDBURL =
+  'mongodb+srv://admin:admin@bookstore.a01rqxr.mongodb.net/book-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
